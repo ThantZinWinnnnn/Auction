38cc85fad8043efd373832f88efeb2a1eb637ea0
thantzinwin <thant.zin.winnnn@gmail.com>
2023-02-21 21:13:09 +0630

    feature : single product and product Detail compoents built

--- frontend/src/components/product/Product.tsx
+++ frontend/src/components/product/Product.tsx
@@ -6,7 +6,7 @@ const Product = () => {
 	return (
 		<Box display={"flex"} sx={{ width: "74%", gap: "3%" }}>
 			<Box sx={{ width: "30%" }}>
-				<Box sx={{}}>
+				<Box sx={{mb:1,paddingX:3}} overflow='hidden'>
 					<Link to={"/detail"}>
 						<img
 							width={"100%"}
@@ -15,7 +15,7 @@ const Product = () => {
 						/>
 					</Link>
 				</Box>
-				<Typography component={"div"}>Thant Zin Win</Typography>
+				<Typography variant="caption" component={"div"} textAlign='center'>Thant Zin Win</Typography>
 			</Box>
 
 			<Box sx={{ width: "50%" }}>
@@ -26,7 +26,11 @@ const Product = () => {
 						fontWeight="bold"
 						variant="subtitle2"
 						component={"div"}
-						sx={{ marginBottom: 2 }}
+						sx={{ marginBottom: 3 ,
+							"&:hover":{
+								color:"primary.light"
+							}
+						}}
 					>
 						No Reserve Pallets of Customer Returns I Small Domestic Appliances,
 						Fashion, Toys & Furniture - Sourced from a Major UK Retailer
@@ -36,19 +40,26 @@ const Product = () => {
 					variant="caption"
 					color={"grey.600"}
 					component="p"
-					sx={{ mb: 2 }}
+					sx={{ mb: 3 }}
 				>
 					All pallets are delivery only.Standard pallets £70+VAT,oversized
 					pallets £140+VAT to the UK exc Highlands,Islands&South-West which are
 					a higher rate
 				</Typography>
-				<Typography variant="subtitle2" component={"h5"} fontWeight={"bold"}>
+				<Typography variant="subtitle2" component={"h5"} fontWeight={"bold"} sx={{mb:1.8}}>
 					Auction dates
 				</Typography>
+				<Typography variant="caption" component={'h6'} marginBottom={"0.5rem"}>Starts: Feb 08, 2023 12:00 PM GMT</Typography>
+				<Typography variant="caption" component={'h6'}>Ends from: Feb 21, 2023 01:00 PM GMT</Typography>
 			</Box>
 			<Box sx={{ width: "17%" }}>
 				<Link to={"/detail"}>
-					<Button fullWidth variant="contained" color="warning">
+					<Button
+						fullWidth
+						variant="contained"
+						color="warning"
+						disableElevation
+					>
 						<Typography fontSize={"12px"}>View Catalog</Typography>
 					</Button>
 				</Link>
