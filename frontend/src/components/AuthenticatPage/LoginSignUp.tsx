bfd562b283cbacbaa3a3e6d17ac0d21fb1945d6e
thantzinwin <thant.zin.winnnn@gmail.com>
2023-03-07 22:25:19 +0630

    feature : login form mobile breakpoints

--- frontend/src/components/AuthenticatPage/LoginSignUp.tsx
+++ frontend/src/components/AuthenticatPage/LoginSignUp.tsx
@@ -7,29 +7,47 @@ import {
   useTheme,
   useMediaQuery,
   Paper,
+  TextField,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
 } from "@mui/material";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import logo from "../../assets/images/logo.svg";
+import * as yup from "yup";
+import { CheckBox } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
-
-interface FormValues{
-  username?:string,
-  email:string,
-  password:string
+interface FormValues {
+  username?: string;
+  email: string;
+  password: string;
 }
 
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).required(),
+});
+
 const LoginSignUp = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: yupResolver(schema),
+  });
+
   const theme = useTheme();
   const desktop = useMediaQuery(theme.breakpoints.up("md"));
 
   return (
-    <Box
-      width={desktop ? "50%" : "100%"}
-      height="100vh"
-      sx={{ p: 2 }}
-    >
-      <Box display={"flex"} alignItems="center" gap={3} mb={12}>
+    <Box width={desktop ? "50%" : "100%"} height="100vh" sx={{ p: 2 }}>
+      <Box display={"flex"} alignItems="center" gap={3} mb={2}>
         <Box sx={{ width: "50px", height: "50px" }} overflow="hidden">
           <img width={"100%"} src={`${logo}`} alt="logo" />
         </Box>
@@ -42,35 +60,130 @@ const LoginSignUp = () => {
           Auctions
         </Typography>
       </Box>
-      <Box sx={{pt:4}}>
-        <Typography textAlign={"center"} fontWeight="bold" sx={{
-          fontSize:{
-            xs:26
-          },
-          mb:{
-            xs:2
-          }
-        }}>
+      <Box sx={{ pt: 4 }}>
+        <Typography
+          textAlign={"center"}
+          fontWeight="bold"
+          sx={{
+            fontSize: {
+              xs: 26,
+            },
+            mb: {
+              xs: 2,
+            },
+          }}
+        >
           Warmly , Welcome back
         </Typography>
         <Typography
           textAlign={"center"}
-          variant="body2"
+          variant="body1"
           fontWeight={"light"}
-          color='#8C8E95'
+          color="#8C8E95"
         >
           Please enter your details.
         </Typography>
-        <Box width={'95%'} mx={'auto'} sx={{
-          mt:12
-        }}>
-          <Box>
-            <Typography sx={{
+        <Box
+          width={"98%"}
+          mx={"auto"}
+          sx={{
+            mt: 10,
+            width:{
+              xs:"98%",
+              sm:"80%"
+            }
+          }}
+        >
+          <form>
+            <Box
+              display={"flex"}
+              flexDirection="column"
+              sx={{
+                gap: 4,
+              }}
+            >
+              <Box>
+                <Typography id="email" sx={{
+                  mb:1,
+                  fontSize:{
+                    xs:18,
+                    sm:20
+                  }
+                }}>Email</Typography>
+                <TextField
+                aria-labelledby="email"
+                type="email"
+                variant="outlined"
+                error={!!errors.email}
+                helperText={errors.email ? errors?.email.message : ""}
+                fullWidth
+              />
+              </Box>
+              
+              <Box>
+                <Typography id="password" fontWeight={'medium'} sx={{
+                  fontSize:{
+                    xs:18,
+                    sm:20
+                  }
+                }}>Password</Typography>
+                <TextField
+                aria-labelledby="password"
+                type="password"
+                variant="outlined"
+                error={!!errors.password}
+                helperText={errors.password ? errors?.password.message : ""}
+                fullWidth
+              />
+              </Box>
+            </Box>
+            <Link to={"/forgotpassword"}>
+              <Typography sx={{ color: "black", textDecoration: "underline",mb:6,mt:2,"&:hover":{
+                color:"blue"
+              } }}>
+                Forgot your password?
+              </Typography>
+            </Link>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox defaultChecked disableRipple disableTouchRipple/>}
+                label={
+                  <Typography textAlign={'left'} sx={{
+                    fontSize:{
+                      xs:14,
+                      sm:16
+                    },
+                    
+                  }}>
+                    I agree to the{" "}
+                    <span style={{ color: "blue" }}>Terms & Conditions</span>{" "}
+                    and <span style={{ color: "blue" }}>Privacy Policy</span>
+                  </Typography>
+                }
+              />
+            </FormGroup>
+            <Button fullWidth variant="contained" sx={{
+              textTransform:"none",
+              bgcolor:"#181818",
+              mt:4,
+              py:1.5,
               fontSize:{
-                xs:18
+                xs:16,
+                sm:18
               }
-            }} component={'div'}>Email</Typography>
-            
+            }}>Sign In</Button>
+          </form>
+
+          <Box display={'flex'} justifyContent='center' alignItems='center' sx={{
+            mt:4
+          }}>
+            <Typography color={'#D1D0CC'}>Don't have an account?</Typography>
+            <Button sx={{
+              textTransform:"none",
+              color:"black",
+              fontSize:16,
+              fontWeight:"bold"
+            }}>Sign Up</Button>
           </Box>
         </Box>
       </Box>
