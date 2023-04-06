d1fe38df8e7f1269481440b39cfdc0d0d814f2ae
thantzinwin <thant.zin.winnnn@gmail.com>
2023-04-07 00:23:16 +0630

    feature: upload image in create product component

--- frontend/src/components/ProfileComponent/components/CreateProduct/CreateProduct.tsx
+++ frontend/src/components/ProfileComponent/components/CreateProduct/CreateProduct.tsx
@@ -1,17 +1,91 @@
-import { Box, Paper, Typography } from '@mui/material'
-import React from 'react'
+import {
+  Box,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+
+import ImageUpload from "../ImageUpload/ImageUpload";
+import { useState } from "react";
+
+
 
 const CreateProduct = () => {
+  const [productImage,setProductImage] = useState('')
+  const theme = useTheme();
+  const mediumScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const Mobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <>
-        <Box component={'form'} onSubmit={()=>{}}>
-            <Paper sx={{boxShadow:"rgb(0 0 0 / 4%) 0px 5px 22px, rgb(0 0 0 / 3%) 0px 0px 0px 0.5px",borderRadius:"20px"}}>
-                <Typography fontWeight={"bold"} variant='h6' component={'h6'}>Product Details</Typography>
-                
-            </Paper>
+      <Box width={Mobile ? "100%" : mediumScreen ? "90%" : "80%"}>
+        <Box mb={4}>
+          <Typography fontWeight={"bold"} variant="h5" component={"h6"}>
+            Create a new product
+          </Typography>
         </Box>
-    </>
-  )
-}
+        <Box component={"form"} onSubmit={() => {}}>
+          <Grid container spacing={3}>
+            <Grid item xl={8}>
+              <Paper
+                sx={{
+                  boxShadow:
+                    "rgb(145 158 171 / 20%) 0px 0px 2px 0px, rgb(145 158 171 / 12%) 0px 12px 24px -4px",
+                  borderRadius: "10px",
+                  p:{
+                    xl:3
+                  }
+                }}
+              >
+                <Box>
+                  <TextField
+                    fullWidth
+                    id="outlined-product-title"
+                    label="Product Name"
+                    InputLabelProps={{
+                      style: {
+                        color: 'black'
+                      }
+                    }}
+                    sx={{
+                      '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+                        border:"1px solid black",
+                        
+                      },
+                      '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        border:"1px solid black",
+                        color:"black"
+                      },
+                      
+                    }}
+                  />
+                </Box>
 
-export default CreateProduct
+                <ImageUpload onChange={(image)=> setProductImage(image)} label="image" disabled value={productImage}/>
+              </Paper>
+            </Grid>
+            <Grid item xl={4}>
+              <Box width={"100%"}  height={"100%"}>
+                <Paper sx={{
+                  boxShadow:
+                    "rgb(145 158 171 / 20%) 0px 0px 2px 0px, rgb(145 158 171 / 12%) 0px 12px 24px -4px",
+                  borderRadius: "10px",
+                  p:{
+                    xl:3
+                  }
+                }}>
+                  <Typography>Product Category</Typography>
+                </Paper>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default CreateProduct;
--- frontend/src/components/ProfileComponent/components/ImageUpload/ImageUpload.tsx
+++ frontend/src/components/ProfileComponent/components/ImageUpload/ImageUpload.tsx
@@ -1,52 +1,120 @@
-import { Box } from '@mui/material';
-import React, { useCallback, useState } from 'react'
-import { useDropzone } from 'react-dropzone'
+import {
+  Box,
+  OutlinedInput,
+  Typography,
+  Input,
+  TextField,
+  InputProps,
+} from "@mui/material";
+import React, { useCallback, useState } from "react";
 
-interface imageUploadProps{
-    onChange:(base64:string)=> void,
-    label:string,
-    value:string,
-    disabled?:boolean
+interface imageUploadProps {
+  onChange: (base64: string) => void;
+  label: string;
+  value: string;
+  disabled?: boolean;
+}
+interface FileInputProps extends InputProps {
+  accept?: string;
 }
 
-const ImageUpload: React.FC<imageUploadProps> = ({
-    onChange,
-    label,
-    value,
-    disabled
-}) => {
+const ImageUpload: React.FC<imageUploadProps> = (
+  { onChange, label, value, disabled },
+  { accept, ...inputProps }: FileInputProps
+) => {
+  // const [base64, setBase64] = useState(value);
+  const [imageFile, setImageFile] = useState<File | undefined>(undefined);
+  const [imageBase64, setImageBase64] = useState("");
 
-    const [base64, setBase64] = useState(value);
+  // const handleChange= useCallback((base64:string)=>{
+  //     onChange(base64)
+  // },[onChange]);
 
-    const handleChange= useCallback((base64:string)=>{
-        onChange(base64)
-    },[onChange]);
+  // const handleDrop = useCallback((files: any) => {
+  //     console.log("running")
+  //     const file = files[0]
+  //     const reader = new FileReader();
+  //     reader.onload = (event: any) => {
+  //       setBase64(event.target.result);
+  //       handleChange(event.target.result);
+  //     };
+  //     reader.readAsDataURL(file);
+  // }, [handleChange])
 
-    const handleDrop = useCallback((files:any)=>{
-        const file = files[0];
+  // const {getRootProps,getInputProps} = useDropzone({
+  //     maxFiles:1,
+  //     onDrop:handleDrop,
+  //     disabled,
+  //     accept:{
+  //         'image/jpeg':[],
+  //         'image/png':[]
+  //     }
+  // })
 
-        const reader = new FileReader();
-        reader.onload=(event:any)=>{
-            setBase64(event.target.result);
-            handleChange(event.target.result)
+  const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageFile(file);
+        if (reader.result) {
+          setImageBase64(reader.result as string);
         }
-    },[handleChange]);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
-    const {getRootProps,getInputProps} = useDropzone({
-        maxFiles:1,
-        onDrop:handleDrop,
-        disabled,
-        accept:{
-            'image/jpeg':[],
-            'image/png':[]
-        }
-    })
+  console.log("src", imageBase64);
 
   return (
-    <Box sx={{}} {...getRootProps()}>
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          p: {
+            xl: 2,
+          },
+          color: "black",
+          border: "1px dotted black",
+          borderRadius: "10px",
+          mt:"15px"
+        }}
+      >
+        <Typography fontWeight={'bold'} variant="h6" mb={1}>Upload Image</Typography>
+        <Input
+          type="file"
+          onChange={handleFileSelect}
+          {...inputProps}
+          inputProps={{ accept: "image/*" }}
+          title="upload"
+          sx={{
+            color:"primary.main",
+            
+          }}
+        />    
+        <Box width={"100%"} height={"300px"} overflow={"hidden"} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+        {imageBase64 ? (
+            <img
+              width={"100%"}
+              height={"100%"}
+              style={{ objectFit: "contain" }}
+              src={imageBase64}
+              alt="Preview"
+            />
+        ):(
+            <Typography fontWeight={'bold'} variant="h4">Please upload the product image</Typography>
+        )}
+      </Box>
+      </Box>
+    </>
+  );
+  {
+    /* <input type='file' {...getInputProps()}/>
+        {base64 ? (<Box display={'flex'} alignItems={'center'} justifyContent={"center"}>
+            <img src={base64} height={"100%"} width={"100%"} alt='Uploaded image'/>
+        </Box>) : <Typography color={'black'}>{label}</Typography>} */
+  }
+};
 
-    </Box>
-  )
-}
-
-export default ImageUpload
+export default ImageUpload;
