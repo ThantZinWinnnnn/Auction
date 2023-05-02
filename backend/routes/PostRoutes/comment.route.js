6c0d20ba6ebe24b6488b1fdd43159add3c0d67e3
thantzinwin <thant.zin.winnnn@gmail.com>
2023-05-02 21:23:55 +0630

    fix: comment route

--- backend/routes/PostRoutes/comment.route.js
+++ backend/routes/PostRoutes/comment.route.js
@@ -1,7 +1,7 @@
 const expres = require("express");
 const router = expres.Router();
 
-const {createComment,updateComment} = require("../../controllers/PostsController/comment.controller")
+const {createComment,updateComment} = require("../../controllers/ProductControllers/comment.controller")
 
 router.route('/createCom/:postId').post(createComment);
 router.route("/update/comment").put(updateComment)
